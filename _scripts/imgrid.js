2b9e1678744d429fde6a7a388cefeab1f69e8dc7
Minee-Liane-Choi <70961403+Minee-Liane-Choi@users.noreply.github.com>
2024-05-23 09:40:41 +0000

    Deploying to gh-pages from @ Minee-Liane-Choi/MineeChoiLab@00841db75e3e95a557cd162f299643940f797771 🚀

--- _scripts/imgrid.js
+++ _scripts/imgrid.js
@@ -1,17 +1,13 @@
-<button onclick="one()">1</button>
-<button onclick="two()">2</button>
-<button onclick="four()">4</button>
-
-<script>
 // Get the elements with class="column"
 var elements = document.getElementsByClassName("column");
 
-// Declare a "loop" variable
+// Declare a loop variable
 var i;
 
 // Full-width images
 function one() {
-  for (i = 0; i < elements.length; i++) {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
     elements[i].style.flex = "100%";
   }
 }
@@ -19,6 +15,7 @@ function one() {
 // Two images side by side
 function two() {
   for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
     elements[i].style.flex = "50%";
   }
 }
@@ -26,7 +23,18 @@ function two() {
 // Four images side by side
 function four() {
   for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
     elements[i].style.flex = "25%";
   }
 }
-</script>
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
